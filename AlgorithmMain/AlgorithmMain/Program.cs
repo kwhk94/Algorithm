87a83c1d990a34458413927aad3772b7578c7af1
wh.kim <whkimKitronyx@kitronyx.com>
2022-08-30 17:12:00 +0900

    DFS 구현

--- AlgorithmMain/AlgorithmMain/Program.cs
+++ AlgorithmMain/AlgorithmMain/Program.cs
@@ -3,8 +3,13 @@
 
 using AlgorithmMain;
 
-BFS_Test bFS_Test = new BFS_Test();
-
-bFS_Test.ShowAllNode();
-
-bFS_Test.SearchNode(0, 7);
+#region ===== BFS Test
+//BFS_Test bFS_Test = new BFS_Test();
+//bFS_Test.ShowAllNode();
+//bFS_Test.SearchNode(0, 7);
+#endregion
+#region===== DFS Test
+DFS_Test dFS_Test = new DFS_Test();
+dFS_Test.ShowAllNode();
+dFS_Test.SearchNode(0, 6);
+#endregion
--- AlgorithmMain/LibraryProject/알고리즘/DFS(깊이탐색).cs
+++ AlgorithmMain/LibraryProject/알고리즘/DFS(깊이탐색).cs
@@ -22,8 +22,6 @@ namespace monoLibrary.알고리즘
             nodeList.ForEach(node => node.marked = false);
 
             stackDFS = new Stack<DataNode<T>>();
-
-            
         }
 
         private bool DFS_Push(DataNode<T> _item)
@@ -33,6 +31,74 @@ namespace monoLibrary.알고리즘
             stackDFS.Push(_item);
             return true;
         }
+        public void SimpleDFS_showAllNode(int _startIndex)
+        {
+            // 스타트 노드 넣기
+            DFS_Push(nodeList[_startIndex]);
 
+            // 스택이 비어질 때 까지 확인
+            while (stackDFS.Count > 0)
+            {
+                var _currentNode = stackDFS.Pop();
+
+                Console.WriteLine(_currentNode.getNodeData.ToString());
+
+                // 인접 노드의 수를 미리 계산하고 for문에 넣어준다.
+                int _adjacenteNodeCount = _currentNode.adjacentNode.Count;
+
+                for (int i = 0; i < _adjacenteNodeCount; i++)
+                {
+                    DFS_Push(_currentNode.adjacentNode[i]);
+                }
+            }
+        }
+        public void DFS_PathSearch(int _startIndex, int _targetIndex)
+        {
+            Console.WriteLine("start Node: " + nodeList[_startIndex].getNodeData.ToString());
+            Console.WriteLine("target Node: " + nodeList[_targetIndex].getNodeData.ToString());
+
+            // 스타트 노드 넣기
+            DFS_Push(nodeList[_startIndex]);
+
+            bool _bSearch = false;
+            // 큐가 비어질 때 까지 확인
+            while (stackDFS.Count > 0 && _bSearch == false)
+            {
+                var _currentNode = stackDFS.Pop();
+
+                // 인접 노드의 수를 미리 계산하고 for문에 넣어준다.
+                int _adjacenteNodeCount = _currentNode.adjacentNode.Count;
+
+                for (int i = 0; i < _adjacenteNodeCount; i++)
+                {
+
+                    if (DFS_Push(_currentNode.adjacentNode[i]))
+                    {
+                        // 부모 노드 등록
+                        _currentNode.adjacentNode[i].setParentNode = _currentNode;
+                    }
+                    if (_currentNode.adjacentNode[i] == nodeList[_targetIndex])
+                    {
+                        _bSearch = true;
+                        break;
+                    }
+                }
+            }
+
+            var _node = nodeList[_targetIndex];
+            int _Length = 0;
+
+            while (_node.getParentNode != null)
+            {
+                Console.WriteLine(_node.getNodeData.ToString());
+                _node = _node.getParentNode;
+                _Length++;
+            }
+            Console.WriteLine(nodeList[_startIndex].getNodeData.ToString()); // 시작 노드 출력
+
+            Console.WriteLine("시작과 끝 거리: " + _Length);
+
+
+        }
     }
 }
