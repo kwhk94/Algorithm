329d385529a716c805bbf3801270e474110fbed4
wh.kim <wh.kim@kitronyx.com>
2023-03-27 21:17:12 +0900

    광물캐기 초안 작성

--- AlgorithmMain/AlgorithmMain/Program.cs
+++ AlgorithmMain/AlgorithmMain/Program.cs
@@ -2,6 +2,7 @@
 
 
 using AlgorithmMain;
+using AlgorithmMain.알고리즘테스트;
 
 #region ===== BFS_Test ======
 //BFS_Test bFS_Test = new BFS_Test();
@@ -12,9 +13,20 @@ using AlgorithmMain;
 #endregion
 
 #region ===== DFS_Test ======
-DFS_Test mDFS_Test = new DFS_Test();
+//DFS_Test mDFS_Test = new DFS_Test();
 
-mDFS_Test.ShowAllNode();
+//mDFS_Test.ShowAllNode();
 
-mDFS_Test.SearchNode(0, 4);
+//mDFS_Test.SearchNode(0, 4);
+#endregion
+
+#region ===== 광물캐기
+
+광물캐기 test = new 광물캐기();
+var t = test.solution(new int[3] { 1, 3, 2 }, new string[] { "diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone" });
+Console.WriteLine(t);
+
+t = test.solution(new int[3] { 0,1,1 }, new string[] { "diamond", "diamond", "diamond", "diamond", "diamond", "iron", "iron", "iron", "iron", "iron", "diamond" });
+Console.WriteLine(t);
+
 #endregion
